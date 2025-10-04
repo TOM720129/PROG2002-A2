bdecfc3c30c6d4d8870c9404ca7819f090b913b8
TOM720129 <163381036+TOM720129@users.noreply.github.com>
2025-10-04 15:20:05 +0800

    create single event API

--- controller/eventController.js
+++ controller/eventController.js
@@ -8,8 +8,9 @@ router.get('/', async (req, res) => {
   try {
     // query all event from db
     const [rows] = await db.query(
-      `SELECT e.*, c.name AS category_name,
+      `SELECT e.*, o.name AS organization_name, c.name AS category_name
        FROM events e
+       JOIN organizations o ON e.org_id = o.id
        JOIN categories c ON e.category_id = c.id
        WHERE e.is_suspended = 0
        ORDER BY e.start_at ASC`
@@ -32,7 +33,7 @@ router.get('/search-events', async (req, res) => {
     const { date, city, categoryId } = req.query;
 
     // create sql
-    let sql = 'SELECT e.*, c.name AS category_name FROM events e JOIN categories c ON e.category_id = c.id';
+    let sql = 'SELECT e.*, o.name AS organization_name, c.name AS category_name FROM events e JOIN organizations o ON e.org_id = o.id JOIN categories c ON e.category_id = c.id';
     const placeholderArr = [];
 
     // date should between start date and end date
@@ -65,6 +66,36 @@ router.get('/search-events', async (req, res) => {
     console.error('Error searching events:', err);
     res.status(500).json({
       error: 'Failed to search events'
+    });
+  }
+});
+
+router.get('/events/:id', async (req, res) => {
+  try {
+    // create sql
+    const [rows] = await db.query(
+      `SELECT e.*, o.name AS organization_name, c.name AS category_name
+       FROM events e
+       JOIN organizations o ON e.org_id = o.id
+       JOIN categories c ON e.category_id = c.id
+       WHERE e.id = ?`,
+      [req.params.id] // get id in request path parameters
+    );
+
+    // response 404 when event not found
+    if (rows.length === 0) {
+      return res.status(404).json({
+        error: 'Event not found'
+      });
+    }
+
+    // response event with json format
+    res.json(rows[0]);
+  } catch (err) {
+    // response 500 status
+    console.error(err);
+    res.status(500).json({
+      error: 'Failed to fetch event details'
     });
   }
 });
